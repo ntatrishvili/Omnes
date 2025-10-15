b7d9226f71ee0b86b183837929fa24dd3732db58
Lilla Barancsuk <barancsuk.lilla@vik.bme.hu>
2025-10-15 21:34:54 +0200

    Add more units to example JSON, Generalize optimizationm Add test for AssignmentExpression

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 
 from app.conversion.pulp_converter import PulpConverter
 from app.model.model import Model
-from app.operation.example_optimization import optimize
+from app.operation.example_optimization import optimize_energy_system
 
 if __name__ == "__main__":
     config = configparser.ConfigParser()
@@ -13,6 +13,6 @@ if __name__ == "__main__":
 
     with open("data/model_config.json", "r") as file:
         config = json.load(file)
-    model = Model.build("model", config, time_set, freq)
+    model = Model.build("model", config, time_set, frequency=freq)
     problem = PulpConverter().convert_model(model)
-    optimize(**problem)
+    optimize_energy_system(**problem)
